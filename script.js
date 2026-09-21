8938f9b988d166649fb757da10a3bcd4059dfb1e
teo <mateocardenascorrea@gmail.com>
2026-09-21 17:57:29 -0500

    Refactor game initialization and enemy setup
    se ha actualizado el scrip

--- script.js
+++ script.js
@@ -17,3 +17,100 @@ let winFlag = false
 let leftPressed = false
 let rightPresed = false
 let moveX = 0
+
+const ENEMY_ROWS = 4
+const ENEMY_COLS = 6
+const ENEMY_SPACING = 12
+const ENEMY_W = 26
+const ENEMY_H = 20
+let enemyDirection = 1
+let enemySpeed = 0.8
+let enemyMoveCounter = 0
+const ENEMY_MOVE_FRAMES = 12
+let shootCooldown = 0
+const SHOOT_DELAY = 14
+
+// inicializacion de enemigos 
+function initEnemies() {
+    enemies = []
+    const startX = 30
+    const startY = 40
+    for (let row  = 0; row  < ENEMY_ROWS; row ++) {
+        for (let col = 0; col < ENEMY_COLS; col++) {
+            enemies.push({
+                x: startX + col * (ENEMY_W + ENEMY_SPACING),
+                Y: startY + row * (ENEMY_W + ENEMY_SPACING),
+                w: ENEMY_W,
+                h: ENEMY_H,
+                alive: true,
+                color: row === 0 ? '#4915D4' : (row === 1 ? '#D4CA15' : '#6bb0d9')
+            })
+            }
+         }
+    enemyDirection = 1
+    enemySpeed = 0.8
+    enemyMoveCounter = 0
+}
+
+//reiniciar
+function resetGame () {
+    player.X = 180
+    bullets = []
+    score = 0
+    gameOver = false
+    winFlag = false 
+    leftPressed = false
+    rightPresed = false 
+    moveX = 0
+    shootCooldown = 0
+    initEnemies ()
+    updateScore ()
+}
+// actualizar puntos
+function updateScore (){
+    scoreSpan.textContent = score
+}
+
+//disparar
+function shootBullet() {
+    if (gameOver || winFlag) return 
+    bullets.push({
+        x: player.x + player.w/2-3,
+        y: player.y - 8,
+        w: 6,
+        h: 14,
+        speed: 5
+    })
+}
+
+// colisiones
+function rectCollide (r1, r2) {
+    return ! (r2.x > r1.x + r1.w || r2.x + r2.w < r1.x || r2.x > r2.w < r1.x || r2.y + r2.h < r1.y);
+}
+
+// funciones de teclado 
+function handleKeyDown (e) {}
+
+function handleKeyUp (e) {}
+
+// funciones para movil
+function handleTouchStart (e) {}
+
+function handleTouchMove(e) {}
+
+function handleTouchEnd (e) {}
+
+
+// eventos
+window.addEventListener('keydown', handleKeyDown)
+window.addEventListener('keyup', handleKeyUp)
+canvas.addEventListener('touchstart', handleTouchStart, { passive: false})
+canvas.addEventListener('touchmove', handleTouchMove, { passive: false})
+canvas.addEventListener('touchsend', handleTouchEnd, { passive: false})
+canvas.addEventListener('contextmenu', (e) => e.preventDefault())
+document.getElementById('resetBtn').addEventListener('click', resetGame)
+
+
+// inicio del juego 
+initEnemies()
+updateScore ()
